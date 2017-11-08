fa26cc0320c3de31f990ee64f430ab107580398d
彼端。 <mwr1998@qq.com>
2017-11-08 23:09:20 +0800

    Divide search and select to two parts

--- chaci.py
+++ chaci.py
@@ -3,6 +3,7 @@
 import requests
 from bs4 import BeautifulSoup
 import json
+import traceback
 import re
 import time
 def getChinaDailyQuery(word):
@@ -39,6 +40,7 @@ def getWordTrans(word):
     result = soup.body.prettify()
     result = result.replace("&amp;", "&") #转回&
     result = result.replace("\xa0", " ") #去除奇怪的无法输出的符号
+    result = result.replace("\xe2", " ")  # 去除奇怪的无法输出的符号
     result = re.sub("<.+>", "", result, count=0, flags=0) #过滤html标签
     result = re.sub("返回查词首页", "", result, count=0, flags=0) #去除最后一句话
     result = re.sub("(?<=\n)(\s+)\n", "", result, count=0, flags=0) #删除空行
@@ -52,33 +54,86 @@ def getGoogleTrans(sen):
     result = result.json()
     return result["text"]
 
-def lookUpOneWord(word):
-    print("\t" + word)
-    wordTrans = getWordTrans(word)
-    for wt in wordTrans:
-        print("\t\t"+ wt)
-    res = getChinaDailyQuery(word)
-    num = 0
-    for r in res:
-        print("\t" + str(num) + ":" + "\t" + r["pretty"])
-        print("\t\t" + r["tran"])
-        num += 1
-    selection = int(input("Please select one: "))
+def inputWords():
+    words = []
+    while 1:
+        word = input("Please input the word you look up (input '0' to end): ")
+        if word == "":
+            continue
+        if word == "0":
+            break
+        words.append(word)
+    num = len(words)
+    print("您输入共%d个词：" % num)
+    for w in words:
+        print(w)
     print("\n")
-    print("\t" + word)
-    for wt in wordTrans:
-        print("\t\t"+ wt)
-    print("\t" + res[selection]["pretty"])
-    print("\t" + res[selection]["tran"])
+    return words
+
+def lookUpWords(words):
+    result = []
+    numWord = len(words)
+    count = 0
+    for word in words:
+        count += 1
+        print("" + word)
+        wordTrans = getWordTrans(word)
+        for wt in wordTrans:
+            print("\t\t" + wt)
+        res = getChinaDailyQuery(word)
+        num = 0
+        for r in res:
+            print("\t" + str(num) + ":" + "\t" + r["pretty"])
+            print("\t\t" + r["tran"])
+            num += 1
+        result.append({"word": word, "wordTrans": wordTrans, "sentence": res})
+        print("【%d/%d】完成单词查询" % (count, numWord))
+    return result
+
+def selectSentenceAndRecord(data):
+    fileName = time.strftime("%Y-%m-%d-%H-%M-%S", time.localtime()) + ".log"
+    count = 0
+    numAll = len(data)
+    all = ""
+    fo = open(fileName, "a+", encoding='utf-8')
+    for d in data:
+        count += 1
+        print("【%d / %d】进度" % (count, numAll))
+        print("\n")
+        print("" + d["word"])
+        for wt in d["wordTrans"]:
+            print("\t\t" + wt)
+        num = 0
+        for r in d["sentence"]:
+            print("\t" + str(num) + ":" + "\t" + r["pretty"])
+            print("\t\t" + r["tran"])
+            num += 1
+        selection = int(input("Please select one: "))
+        result = "\n" + "\n"
+        result += "" + d["word"] + "\n"
+        for wt in d["wordTrans"]:
+            result += "\t\t" + wt + "\n"
+        result += "\t" + d["sentence"][selection]["pretty"] + "\n"
+        result += "\t" + d["sentence"][selection]["tran"] + "\n"
+        # print(result)
+        all += result
+        fo.write(result)
+    fo.close()
+    return all
+
 
 # getChinaDailyQuery("imply")
 # getWordTrans("submit")
 # getGoogleTrans("Most of the basic models for these objects imply that they are composed almost entirely of neutrons.")
 while 1:
     try:
-        word = input("Please input the word you look up: ")
-        lookUpOneWord(word)
-    except:
+        words = inputWords()
+        data =lookUpWords(words)
+        all = selectSentenceAndRecord(data)
+        print(all)
+    except Exception as e:
         print("发生异常!")
+        print(e)
+        print('traceback:\n%s' % traceback.format_exc())
     finally:
         print("\n\n")
